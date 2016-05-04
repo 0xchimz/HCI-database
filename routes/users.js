fdc67f093b943f287049ad16f15190b699e0cc67
ChinCluBi <chincub@gmail.com>
2016-05-04 11:13:41 +0700

    Update user when request new properties

--- routes/users.js
+++ routes/users.js
@@ -13,15 +13,60 @@ router.get('/:stdId/:collection', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      return next(err)
+      next(err)
     }
     if (users.length !== 1) {
       var error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
       next(error)
-    } else {
-      res.json(users[0].data[collection])
     }
+    res.json(users[0].data[collection])
+  }
+})
+
+router.post('/:stdId/:collection', function (req, res, next) {
+  var _id = req.params.stdId
+  var pin = req.param('pin')
+
+  var collection = req.params.collection
+  var newData = req.body[collection]
+
+  User.find({_id: _id, pin: pin}, authUser)
+
+  function authUser (err, users) {
+    if (err) {
+      next(err)
+    }
+    if (!newData) {
+      var error = new Error('Cannot found your collection')
+      error.status = 400
+      next(error)
+    }
+    if (users.length !== 1) {
+      error = new Error('Student ID and PIN are incorrect.')
+      error.status = 404
+      next(error)
+    }
+    console.log(newData)
+    var userData = users[0].data[collection]
+    console.log(users[0].data[collection])
+    if (!userData) {
+      userData = {}
+    }
+    var mergeData = { }
+    mergeData[collection] = Object.assign(newData, userData)
+    var set = { data: mergeData }
+
+    User.update(
+      {_id: _id, pin: pin},
+      { $set: set},
+      function (err, result) {
+        if (err) {
+          next(err)
+        }
+        res.json(result)
+      }
+    )
   }
 })
 
@@ -32,15 +77,41 @@ router.get('/:stdId', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      return next(err)
+      next(err)
     }
     if (users.length !== 1) {
       var error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
       next(error)
-    } else {
-      res.json(users[0].data)
     }
+    res.json(users[0].data)
+  }
+})
+
+router.post('/:stdId', function (req, res, next) {
+  var _id = req.params.stdId
+  var pin = req.param('pin')
+
+  var body = req.body
+  console.log('/:stdId')
+  User.find({_id: _id, pin: pin}, authUser)
+
+  function authUser (err, users) {
+    if (err) {
+      next(err)
+    }
+    if (users.length !== 1) {
+      var error = new Error('Student ID and PIN are incorrect.')
+      error.status = 404
+      next(error)
+    }
+    var set = Object.assign(users[0].data, body)
+    User.findByIdAndUpdate({_id: _id}, {$set: {data: set}}, function (err, user) {
+      if (err) {
+        next(err)
+      }
+      res.json(users[0].data)
+    })
   }
 })
 
