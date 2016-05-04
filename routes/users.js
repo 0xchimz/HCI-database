9818d7c7359cc7e001ddbd316d1b48abf29bf76c
ChinCluBi <chincub@gmail.com>
2016-05-04 17:06:53 +0700

    Fix bug: http server

--- routes/users.js
+++ routes/users.js
@@ -20,6 +20,8 @@ router.get('/:stdId/:collection', function (req, res, next) {
       error.status = 404
       return next(error)
     }
+    var data = users[0].data[collection]
+    data.stdId = collection
     res.json(users[0].data[collection])
   }
 })
@@ -64,52 +66,6 @@ router.delete('/:stdId/:collection', function (req, res, next) {
   }
 })
 
-router.post('/:stdId/:collection', function (req, res, next) {
-  var _id = req.params.stdId
-  var pin = req.param('pin')
-
-  var collection = req.params.collection
-  var newData = req.body[collection]
-
-  User.find({_id: _id, pin: pin}, authUser)
-
-  function authUser (err, users) {
-    if (err) {
-      return next(err)
-    }
-    if (!newData) {
-      var error = new Error('Cannot found your collection')
-      error.status = 400
-      return next(error)
-    }
-    if (users.length !== 1) {
-      error = new Error('Student ID and PIN are incorrect.')
-      error.status = 404
-      return next(error)
-    }
-    console.log(newData)
-    var userData = users[0].data[collection]
-    console.log(users[0].data[collection])
-    if (!userData) {
-      userData = {}
-    }
-    var mergeData = { }
-    mergeData[collection] = Object.assign(newData, userData)
-    var set = { data: mergeData }
-
-    User.update(
-      {_id: _id, pin: pin},
-      { $set: set},
-      function (err, result) {
-        if (err) {
-          return next(err)
-        }
-        res.json(result)
-      }
-    )
-  }
-})
-
 router.get('/:stdId', function (req, res, next) {
   var _id = req.params.stdId
   var pin = req.param('pin')
@@ -133,7 +89,6 @@ router.post('/:stdId', function (req, res, next) {
   var pin = req.param('pin')
 
   var body = req.body
-  console.log('/:stdId')
   User.find({_id: _id, pin: pin}, authUser)
 
   function authUser (err, users) {
@@ -153,12 +108,6 @@ router.post('/:stdId', function (req, res, next) {
       res.json(users[0].data)
     })
   }
-})
-
-router.get('/', function (req, res, next) {
-  User.find({}, function (err, users) {
-    res.json(users)
-  })
 })
 
 module.exports = router
