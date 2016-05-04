a6e3e376ad1a49495fe81075b8628849a3b2592b
ChinCluBi <chincub@gmail.com>
2016-05-04 12:03:14 +0700

    Add delete method

--- routes/users.js
+++ routes/users.js
@@ -13,14 +13,54 @@ router.get('/:stdId/:collection', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      next(err)
+      return next(err)
     }
     if (users.length !== 1) {
       var error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
-      next(error)
+      return next(error)
     }
     res.json(users[0].data[collection])
+  }
+})
+
+router.delete('/:stdId/:collection', function (req, res, next) {
+  var _id = req.params.stdId
+  var pin = req.param('pin')
+
+  var collection = req.params.collection
+
+  User.find({_id: _id, pin: pin}, authUser)
+
+  function authUser (err, users) {
+    if (err) {
+      return next(err)
+    }
+    if (users.length !== 1) {
+      var error = new Error('Student ID and PIN are incorrect.')
+      error.status = 404
+      return next(error)
+    }
+    var userData = users[0].data
+    if (!userData[collection]) {
+      error = new Error('Record not found.')
+      error.status = 401
+      return next(error)
+    }
+    delete userData[collection]
+    var set = {data: userData}
+    User.update(
+      {_id: _id, pin: pin},
+      { $set: set},
+      function (err, result) {
+        if (err) {
+          return next(err)
+        }
+        res.json('updated', {
+          message: 'Record [' + collection + '] is deleted'
+        })
+      }
+    )
   }
 })
 
@@ -35,17 +75,17 @@ router.post('/:stdId/:collection', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      next(err)
+      return next(err)
     }
     if (!newData) {
       var error = new Error('Cannot found your collection')
       error.status = 400
-      next(error)
+      return next(error)
     }
     if (users.length !== 1) {
       error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
-      next(error)
+      return next(error)
     }
     console.log(newData)
     var userData = users[0].data[collection]
@@ -62,7 +102,7 @@ router.post('/:stdId/:collection', function (req, res, next) {
       { $set: set},
       function (err, result) {
         if (err) {
-          next(err)
+          return next(err)
         }
         res.json(result)
       }
@@ -77,12 +117,12 @@ router.get('/:stdId', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      next(err)
+      return next(err)
     }
     if (users.length !== 1) {
       var error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
-      next(error)
+      return next(error)
     }
     res.json(users[0].data)
   }
@@ -98,17 +138,17 @@ router.post('/:stdId', function (req, res, next) {
 
   function authUser (err, users) {
     if (err) {
-      next(err)
+      return next(err)
     }
     if (users.length !== 1) {
       var error = new Error('Student ID and PIN are incorrect.')
       error.status = 404
-      next(error)
+      return next(error)
     }
     var set = Object.assign(users[0].data, body)
     User.findByIdAndUpdate({_id: _id}, {$set: {data: set}}, function (err, user) {
       if (err) {
-        next(err)
+        return next(err)
       }
       res.json(users[0].data)
     })
