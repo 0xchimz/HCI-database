95370875989c1aa191786f959f89af6a3455d851
ChinCluBi <chincub@gmail.com>
2016-05-07 14:02:27 +0700

    Fix allow method for delete

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.set('view engine', 'jade')
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
